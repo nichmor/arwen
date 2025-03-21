8415c64a72a3fcf432be7d11a62cc2ad5d12166a
nichmor <nmorkotilo@gmail.com>
2025-03-21 14:26:50 +0200

    misc: test arwen with cargo

--- tests/rust_integration/macho/common.rs
+++ tests/rust_integration/macho/common.rs
@@ -9,11 +9,13 @@ pub fn run_command(cmd: &str, args: &[&str]) -> std::io::Result<Output> {
     Ok(output)
 }
 
+#[cfg(target_os = "macos")]
 pub(crate) fn codesign_binary(base_binary: &str) -> std::io::Result<()> {
     run_command("codesign", &["--force", "--sign", "-", base_binary]).unwrap();
     Ok(())
 }
 
+#[cfg(target_os = "macos")]
 pub(crate) fn calculate_md5_hash(base_binary: &str) -> std::io::Result<String> {
     let md5_output = Command::new("md5").arg(base_binary).output().unwrap();
     let md5_hash = String::from_utf8_lossy(&md5_output.stdout)
